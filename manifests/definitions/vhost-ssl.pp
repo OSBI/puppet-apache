cad1f9bcfee1653dc1208bf6abaa3eb39447498b
Tom Barber <tom@analytical-labs.com>
2011-11-18 10:49:22 +0000

    try to fix file

--- manifests/definitions/vhost-ssl.pp
+++ manifests/definitions/vhost-ssl.pp
@@ -290,21 +290,21 @@ define apache::vhost-ssl (
 
     # put a copy of the CSR in htdocs, or another location if $publish_csr
     # specifies so.
-    file { "public CSR file for $name":
-      ensure  => $publish_csr ? {
-        false   => "absent",
-        default => "present",
-      },
-      path    => $publish_csr ? {
-        true    => "${apache::params::root}/${name}/htdocs/${name}.csr",
-        false   => "${apache::params::root}/${name}/htdocs/${name}.csr",
-        default => $publish_csr,
-      },
-      source  => "file://$csrfile",
-      mode    => 640,
-      seltype => "httpd_sys_content_t",
-     require => File[$csrfile],
-    }
+#    file { "public CSR file for $name":
+#      ensure  => $publish_csr ? {
+#        false   => "absent",
+#        default => "present",
+#      },
+#      path    => $publish_csr ? {
+#        true    => "${apache::params::root}/${name}/htdocs/${name}.csr",
+#        false   => "${apache::params::root}/${name}/htdocs/${name}.csr",
+#        default => $publish_csr,
+#      },
+#      source  => "file://$csrfile",
+#      mode    => 640,
+#      seltype => "httpd_sys_content_t",
+#     require => File[$csrfile],
+#    }
     
   }
 }
